3ec756fb65284df60da63f40644fe40d7f3f52e2
manjulats <manjula.thirugnanasambandam@gmail.com>
2015-12-13 22:47:48 +0530

    Course Project 1 - Final (Plot3 and Plot4)

--- plot3.R
+++ plot3.R
@@ -9,22 +9,16 @@ plot3 <- function()
 	## Finding the range for metering
 	metering_range <- range(0,  h_power_cons$Sub_metering_1,  h_power_cons$Sub_metering_2,  h_power_cons$Sub_metering_3)
 
-	## Drawing the Graph, with the line for Sub_metering_1
-	plot(h_power_cons$Sub_metering_1, type="l", ylim=metering_range, axes=FALSE, ann=FALSE)	
-
-	## Setting the Axis
-	axis(1, at=1:3, lab=c("Thu", "Fri", "Sat"))
-	axis(2, las=1, at=10*0:metering_range[2])
-
-	box()		## Drawing a box
+	## Drawing the Graph, with the line for Sub_metering_1	
+	plot(strptime(paste(h_power_cons$Date, h_power_cons$Time, sep=" "), "%d/%m/%Y %H:%M:%S"),h_power_cons$Sub_metering_1, type="l", ylim=metering_range, ann=FALSE)	
 
 	## Adding the lines for the Sub_metering_2 and Sub_metering_3
-	lines(h_power_cons$Sub_metering_2, type="l", col="red")
-	lines(h_power_cons$Sub_metering_3, type="l", col="blue")
+	lines(strptime(paste(h_power_cons$Date, h_power_cons$Time, sep=" "), "%d/%m/%Y %H:%M:%S"),h_power_cons$Sub_metering_2, type="l", col="red")
+	lines(strptime(paste(h_power_cons$Date, h_power_cons$Time, sep=" "), "%d/%m/%Y %H:%M:%S"),h_power_cons$Sub_metering_3, type="l", col="blue")
 
 	## Setting the Title and Legend
-	title(ylab="Energy sub metering")	
-	legend(1, metering_range[2], c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.8, col=c("black","red","blue"),lty=1, xjust=-2.2);
+	title(ylab="Energy sub metering")
+	legend("topright", inset=.05, c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"),lty=1)
 
 	dev.copy(png, file="plot3.png") ## Copy the Graph to a PNG File
 	dev.off()  ## Closing the PNG Device
